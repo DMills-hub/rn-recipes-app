af32f743644ed8ff2b7249f4719d588a44f6c43f
Dan Mills <maildanmills@gmail.com>
2020-05-27 00:16:04 +0100

    Registration logic done

---
+++ components/Authenticate/Authenticate.js
@@ -0,0 +1,136 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Platform,
+  Text,
+  Button,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from '../../constants/Colors';
+
+const Authenticate = (props) => {
+  return (
+    <View style={styles.authScreen}>
+      <View style={styles.authContainer}>
+        <View style={styles.formController}>
+          <View style={styles.labelConatiner}>
+            <Text style={styles.label}>Username</Text>
+          </View>
+          <View style={styles.formControls}>
+            <Ionicons
+              size={20}
+              name={Platform.OS === "android" ? "md-person" : "ios-person"}
+            />
+            <TextInput
+              style={styles.textInput}
+              value={props.username}
+              onChangeText={props.onUsernameChange}
+            />
+          </View>
+        </View>
+        <View style={styles.formController}>
+          <View style={styles.labelConatiner}>
+            <Text style={styles.label}>Password</Text>
+          </View>
+          <View style={styles.formControls}>
+            <Ionicons
+              size={20}
+              name={Platform.OS === "android" ? "md-key" : "ios-key"}
+            />
+            <TextInput
+              style={styles.textInput}
+              value={props.password}
+              onChangeText={props.onPasswordChange}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        {!props.loginMode ? (
+          <View style={styles.formController}>
+            <View style={styles.labelConatiner}>
+              <Text style={styles.label}>Confirm Password</Text>
+            </View>
+            <View style={styles.formControls}>
+              <Ionicons
+                size={20}
+                name={Platform.OS === "android" ? "md-key" : "ios-key"}
+              />
+              <TextInput
+                style={styles.textInput}
+                value={props.confirmPassword}
+                onChangeText={props.onConfirmPasswordChange}
+                secureTextEntry={true}
+              />
+            </View>
+          </View>
+        ) : null}
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title={props.loginMode ? "Login" : "Register"}
+              onPress={props.onSubmit}
+              color={Colors.success}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title={`Switch to ${props.loginMode ? "Register" : "Login"}`}
+              onPress={props.changeMode}
+              color={Colors.primary}
+            />
+          </View>
+        </View>
+        {props.error.error ? <Text>{props.error.message}</Text> : null}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  authScreen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  authContainer: {
+    height: 300,
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formController: {
+    width: "100%",
+    padding: 30,
+  },
+  textInput: {
+    width: "90%",
+    marginLeft: 10,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  formControls: {
+    flexDirection: "row",
+  },
+  labelConatiner: {
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: '70%'
+  },
+  button: {
+    width: '40%',
+    minWidth: 100
+  },
+});
+
+export default Authenticate;
--- screens/auth/Auth.js
+++ screens/auth/Auth.js
@@ -1,12 +1,91 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import Authenticate from "../../components/Authenticate/Authenticate";
+import ENVS from "../../env";
 
-const Auth = props => {
+const Auth = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const [mode, setMode] = useState(true);
+
+  const changeModeHandler = () => {
+    setMode((prevState) => !prevState);
+  };
+
+  const onUsernameChangeHandler = (text) => {
+    setUsername(text);
+  };
+
+  const onPasswordChangeHandler = (text) => {
+    setPassword(text);
+  };
+
+  const onConfirmPasswordChangeHandler = (text) => {
+    setConfirmPassword(text);
+  };
+
+  const onRegisterHandler = async () => {
+    if (
+      username.trim() === "" ||
+      password.trim() === "" ||
+      confirmPassword.trim() === ""
+    )
+      return setError({ error: true, message: "No blank values." });
+    if (password !== confirmPassword)
+      return setError({ error: true, message: `Password's don't match.` });
+    try {
+      const result = await fetch(`${ENVS.url}/auth/register`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          confirmPassword: confirmPassword,
+        }),
+      });
+      const registeredAttempt = await result.json();
+      if (registeredAttempt.success) {
+        setMode(true);
+        setError({
+          error: false,
+          message: "",
+        });
+        setPassword("");
+        setConfirmPassword("");
+        return;
+      }
+      setError({
+        error: true,
+        message: registeredAttempt.error,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onLoginHandler = () => {};
+
   return (
-    <View>
-      <Text>Auth Screen</Text>
-    </View>
-  )
-}
+    <Authenticate
+      onUsernameChange={onUsernameChangeHandler}
+      onPasswordChange={onPasswordChangeHandler}
+      onConfirmPasswordChange={onConfirmPasswordChangeHandler}
+      onSubmit={mode ? () => console.log("Login") : onRegisterHandler}
+      username={username}
+      password={password}
+      confirmPassword={confirmPassword}
+      error={error}
+      loginMode={mode}
+      changeMode={changeModeHandler}
+    />
+  );
+};
 
 export default Auth;
